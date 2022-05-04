2b577871ba67a22ba5182b34b54fc40c372865a9
Marianela Gambina <marianela.gambina@mulesoft.com>
2022-05-04 17:46:04 -0300

    updating readme

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -4,6 +4,5 @@ const booksController = require('../controllers/booksController')
 const booksRouter = express.Router();
 
 booksRouter.get('/books', booksController.getAll);
-// booksRouter.post('/book', booksController.create);
 
 module.exports = booksRouter;
